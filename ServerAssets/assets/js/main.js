3aa95deef6a08750998442fb50d474135754990b
Gavin Barnett <gavbarnett@gmail.com>
2018-07-12 22:15:16 +0100

    added responsive re-size

--- ServerAssets/assets/js/main.js
+++ ServerAssets/assets/js/main.js
@@ -67,6 +67,9 @@ function runTestGame(){
 }
 
 function controls(){
+    window.onresize = function(e) {
+        resize()
+    }
     document.onkeydown = function(e) {
         e = e || window.event
         if (e.keyCode == 32) { //spacebar
